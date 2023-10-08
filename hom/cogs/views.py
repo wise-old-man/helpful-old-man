1e8d8194cb4d867ecf2692540f271d3dc2f7a520
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2023-10-08 09:14:19 -0700

    Migrate utilites and update views

--- hom/cogs/views.py
+++ hom/cogs/views.py
@@ -1,10 +1,11 @@
+import discord
 from discord.ext import commands
 
-import discord
-import utilities.discord_utils as du
+from hom import utils
+from hom.config import Constants
 
 
-class PViewSupport(discord.ui.View):
+class Support(discord.ui.View):
     def __init__(self) -> None:
         super().__init__(timeout=None)
 
@@ -14,10 +15,10 @@ class PViewSupport(discord.ui.View):
         custom_id="persistent_view:groups_instructions",
     )
     async def groups_instructions(
-        self, interaction: discord.Interaction, button: discord.ui.Button
+        self, interaction: discord.Interaction, _: discord.ui.Button["Support"]
     ) -> None:
         await interaction.response.send_message(
-            view=PViewSupport_Group(),
+            view=SupportGroup(),
             content="What do you like assistance with?",
             ephemeral=True,
         )
@@ -28,10 +29,10 @@ class PViewSupport(discord.ui.View):
         custom_id="persistent_view:names_instructions",
     )
     async def names_instructions(
-        self, interaction: discord.Interaction, button: discord.ui.Button
+        self, interaction: discord.Interaction, _: discord.ui.Button["Support"]
     ) -> None:
         await interaction.response.send_message(
-            view=PViewSupport_Names(),
+            view=SupportNames(),
             content="What do you like assistance with?",
             ephemeral=True,
         )
@@ -41,10 +42,12 @@ class PViewSupport(discord.ui.View):
         style=discord.ButtonStyle.green,
         custom_id="persistent_view:api_key",
     )
-    async def api_key(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
+    async def api_key(
+        self, interaction: discord.Interaction, button: discord.ui.Button["Support"]
+    ) -> None:
         await interaction.response.defer()
         instructions = f"If you'd like to get an API Key, please tell us your project's name and we'll create you a new API key.\n\n{cd.view_footer}"
-        await du.create_ticket_for_user(interaction, instructions, button.label)
+        await utils.create_ticket_for_user(interaction, instructions, button.label)
 
     @discord.ui.button(
         label="Other",
@@ -52,14 +55,14 @@ class PViewSupport(discord.ui.View):
         custom_id="persistent_view:other_instructions",
     )
     async def other_instructions(
-        self, interaction: discord.Interaction, button: discord.ui.Button
+        self, interaction: discord.Interaction, button: discord.ui.Button["Support"]
     ) -> None:
         await interaction.response.defer()
-        instructions = f"Explain what you require assistance with below.\n\n{cd.view_footer}"
-        await du.create_ticket_for_user(interaction, instructions, f"Other")
+        instructions = f"Explain what you require assistance with below.\n\n{Constants.FOOTER}"
+        await utils.create_ticket_for_user(interaction, instructions, f"Other")
 
 
-class PViewVerify(discord.ui.View):
+class Verify(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
 
@@ -69,15 +72,17 @@ class PViewVerify(discord.ui.View):
         custom_id="persistent_view:verify_yes",
     )
     async def verify_yes(
-        self, interaction: discord.Interaction, button: discord.ui.Button
+        self, interaction: discord.Interaction, button: discord.ui.Button["Verify"]
     ) -> None:
         await interaction.response.send_message(
-            content=f"{cd.status_emoji_complete} Closing this ticket.", ephemeral=True
+            content=f"{Constants.COMPLETE} Closing this ticket.", ephemeral=True
         )
+
+        assert isinstance(interaction.channel, discord.guild.GuildChannel)
         await interaction.channel.delete()
 
 
-class PViewSupportGroup(discord.ui.View):
+class SupportGroup(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
 
@@ -87,14 +92,23 @@ class PViewSupportGroup(discord.ui.View):
         custom_id="persistent_view:group_verify",
     )
     async def group_verify(
-        self, interaction: discord.Interaction, button: discord.ui.Button
+        self, interaction: discord.Interaction, button: discord.ui.Button["SupportGroup"]
     ) -> None:
+        instructions = (
+            "To verify your group please provide a screenshot to prove ownership. We have "
+            "attached an example of what we need to see below. The screenshot must "
+            "contain:\n\n- Your Wise Old Man group ID (found in that group’s page URL), "
+            "your Discord ID, and today’s date typed into your in-game chatbox.\n- Your Clan "
+            "tab open showing your username and rank. For clans, you must be Owner or Deputy "
+            "Owner to verify the group. For the old clan chat, you must be Owner or General "
+            f"(gold star).\n\n{Constants.FOOTER}"
+        )
+
         await interaction.response.defer()
-        instructions = f"To verify your group please provide a screenshot to prove ownership. We have attached an example of what we need to see below. The screenshot must contain:\n\n- Your Wise Old Man group ID (found in that group’s page URL), your Discord ID, and today’s date typed into your in-game chatbox.\n- Your Clan tab open showing your username and rank. For clans, you must be Owner or Deputy Owner to verify the group. For the old clan chat, you must be Owner or General (gold star).\n\n{cd.view_footer}"
-        await du.create_ticket_for_user(
+        await utils.create_ticket_for_user(
             interaction,
             instructions,
-            f"Group {cd.ARROW} " + button.label,
+            f"Group {Constants.ARROW} {button.label}",
             "https://cdn.discordapp.com/attachments/1125434806411464867/1127741141152960542/image.png",
         )
 
@@ -104,14 +118,25 @@ class PViewSupportGroup(discord.ui.View):
         custom_id="persistent_view:group_reset_code",
     )
     async def group_reset_code(
-        self, interaction: discord.Interaction, button: discord.ui.Button
+        self, interaction: discord.Interaction, button: discord.ui.Button["SupportGroup"]
     ) -> None:
+        instructions = (
+            "To reset your verification code please provide a screenshot to prove ownership. "
+            "We have attached an example of what we need to see below. The screenshot must "
+            "contain:\n\n- Your Wise Old Man group ID (found in that group’s page URL), your "
+            "Discord ID, and today’s date typed into your in-game chatbox.\n- Your Clan tab "
+            "open showing your username and rank. For clans, you must be Owner or Deputy Owner "
+            "to verify the group. For the old clan chat, you must be Owner or General (gold star)."
+            "\n\nKeep in mind that verification codes should be secret, they can be used to edit "
+            "or delete a group, so please be mindful of who you choose to share it with.\n\n"
+            f"{Constants.FOOTER}"
+        )
+
         await interaction.response.defer()
-        instructions = f"To reset your verification code please provide a screenshot to prove ownership. We have attached an example of what we need to see below. The screenshot must contain:\n\n- Your Wise Old Man group ID (found in that group’s page URL),  your Discord ID, and today’s date typed into your in-game chatbox.\n- Your Clan tab open showing your username and rank. For clans, you must be Owner or Deputy Owner to verify the group. For the old clan chat, you must be Owner or General (gold star).\n\nKeep in mind that verification codes should be secret, they can be used to edit or delete a group, so please be mindful of who you choose to share it with.\n\n{cd.view_footer}"
-        await du.create_ticket_for_user(
+        await utils.create_ticket_for_user(
             interaction,
             instructions,
-            f"Group {cd.ARROW} " + button.label,
+            f"Group {Constants.ARROW} {button.label}",
             "https://cdn.discordapp.com/attachments/1125434806411464867/1127741141152960542/image.png",
         )
 
@@ -121,14 +146,19 @@ class PViewSupportGroup(discord.ui.View):
         custom_id="persistent_view:group_remove",
     )
     async def group_remove(
-        self, interaction: discord.Interaction, button: discord.ui.Button
+        self, interaction: discord.Interaction, button: discord.ui.Button["SupportGroup"]
     ) -> None:
+        instructions = (
+            "To remove yourself from a group, please provide us with a screenshot containing:"
+            "\n\n- Your in-game username\n- Your Discord username/ID\n- Today's date\n\n"
+            f"{Constants.FOOTER}"
+        )
+
         await interaction.response.defer()
-        instructions = f"To remove yourself from a group, please provide us with a screenshot containing:\n\n- Your in-game username\n- Your Discord username/ID\n- Today's date\n\n{cd.view_footer}"
-        await du.create_ticket_for_user(
+        await utils.create_ticket_for_user(
             interaction,
             instructions,
-            f"Group {cd.ARROW} " + button.label,
+            f"Group {Constants.ARROW} {button.label}",
             "https://cdn.discordapp.com/attachments/1125434806411464867/1129178517557481585/image.png",
         )
 
@@ -138,16 +168,17 @@ class PViewSupportGroup(discord.ui.View):
         custom_id="persistent_view:group_other",
     )
     async def group_other(
-        self, interaction: discord.Interaction, button: discord.ui.Button
+        self, interaction: discord.Interaction, button: discord.ui.Button["SupportGroup"]
     ) -> None:
+        instructions = f"Explain what you require assistance with below.\n\n{Constants.FOOTER}"
+
         await interaction.response.defer()
-        instructions = f"Explain what you require assistance with below.\n\n{cd.view_footer}"
-        await du.create_ticket_for_user(
-            interaction, instructions, f"Group {cd.ARROW} " + button.label
+        await utils.create_ticket_for_user(
+            interaction, instructions, f"Group {Constants.ARROW} {button.label}"
         )
 
 
-class PViewSupportNames(discord.ui.View):
+class SupportNames(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
 
@@ -157,12 +188,20 @@ class PViewSupportNames(discord.ui.View):
         custom_id="persistent_view:names_approve",
     )
     async def names_approve(
-        self, interaction: discord.Interaction, button: discord.ui.Button
+        self, interaction: discord.Interaction, button: discord.ui.Button["SupportNames"]
     ) -> None:
+        instructions = (
+            "Some name changes get skipped, as they can’t be auto-approved by our system and "
+            "require manual approval.\n\nIf yours hasn’t been auto-approved, please tell us the "
+            "name change ID and we’ll manually review it for you.\n\nNote: If you’d like to know "
+            "why your name change has been skipped you can visit our beta website (work in "
+            "progress) at https://beta.wiseoldman.net/names and hover your cursor over the your "
+            f"name change's ℹ️ icon.\n\n{Constants.FOOTER}"
+        )
+
         await interaction.response.defer()
-        instructions = f"Some name changes get skipped, as they can’t be auto-approved by our system and require manual approval.\n\nIf yours hasn’t been auto-approved, please tell us the name change ID and we’ll manually review it for you.\n\nNote: If you’d like to know why your name change has been skipped you can visit our beta website  (work in progress) at https://beta.wiseoldman.net/names and hover your cursor over the your name change's ℹ️ icon. \n\n{cd.view_footer}"
-        await du.create_ticket_for_user(
-            interaction, instructions, f"Names {cd.ARROW} " + button.label
+        await utils.create_ticket_for_user(
+            interaction, instructions, f"Names {Constants.ARROW} {button.label}"
         )
 
     @discord.ui.button(
@@ -171,14 +210,18 @@ class PViewSupportNames(discord.ui.View):
         custom_id="persistent_view:names_delete",
     )
     async def names_delete(
-        self, interaction: discord.Interaction, button: discord.ui.Button
+        self, interaction: discord.Interaction, button: discord.ui.Button["SupportNames"]
     ) -> None:
+        instructions = (
+            "To request a name change history deletion, please provide us with:\n\n- Your in-game "
+            f"username\n- Your Discord username/ID\n- Today's date\n\n{Constants.FOOTER}"
+        )
+
         await interaction.response.defer()
-        instructions = f"To request a name change history deletion, please provide us with:\n\n- Your in-game username\n- Your Discord username/ID\n- Today's date\n\n{cd.view_footer}"
-        await du.create_ticket_for_user(
+        await utils.create_ticket_for_user(
             interaction,
             instructions,
-            f"Names {cd.ARROW} " + button.label,
+            f"Names {Constants.ARROW} {button.label}",
             "https://cdn.discordapp.com/attachments/1125434806411464867/1129178517557481585/image.png",
         )
 
@@ -188,16 +231,17 @@ class PViewSupportNames(discord.ui.View):
         custom_id="persistent_view:names_other",
     )
     async def names_other(
-        self, interaction: discord.Interaction, button: discord.ui.Button
+        self, interaction: discord.Interaction, button: discord.ui.Button["SupportNames"]
     ) -> None:
+        instructions = f"Explain what you require assistance with below.\n\n{Constants.FOOTER}"
+
         await interaction.response.defer()
-        instructions = f"Explain what you require assistance with below.\n\n{cd.view_footer}"
-        await du.create_ticket_for_user(
-            interaction, instructions, f"Names {cd.ARROW} " + button.label
+        await utils.create_ticket_for_user(
+            interaction, instructions, f"Names {Constants.ARROW} {button.label}"
         )
 
 
-class PViewSupportMessage(discord.ui.View):
+class SupportMessage(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
 
@@ -208,33 +252,41 @@ class PViewSupportMessage(discord.ui.View):
         custom_id="persistent_view:message_close",
     )
     async def message_close(
-        self, interaction: discord.Interaction, button: discord.ui.Button
+        self, interaction: discord.Interaction, _: discord.ui.Button["SupportMessage"]
     ) -> None:
+        embed = discord.Embed(description=f"{interaction.user.mention} has closed the ticket.")
+
+        assert isinstance(interaction.channel, discord.guild.GuildChannel)
+        assert isinstance(interaction.user, discord.Member)
         await interaction.response.defer()
         await interaction.channel.set_permissions(interaction.user, overwrite=None)
-        embed = discord.Embed(description=f"{interaction.user.mention} has closed the ticket.")
         await interaction.followup.send(
-            ephemeral=False, embed=embed, view=PViewSupport_Message_Close_Channel()
+            ephemeral=False, embed=embed, view=SupportMessageCloseChannel()
         )
 
 
-class PViewSupportMessageCloseChannel(discord.ui.View):
+class SupportMessageCloseChannel(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
 
     @discord.ui.button(
-        emoji=cd.status_emoji_denied,
+        emoji=Constants.DENIED,
         label="Close Channel",
         style=discord.ButtonStyle.blurple,
         custom_id="persistent_view:message_close_channel",
     )
     async def message_close_channel(
-        self, interaction: discord.Interaction, button: discord.ui.Button
+        self,
+        interaction: discord.Interaction,
+        _: discord.ui.Button["SupportMessageCloseChannel"],
     ) -> None:
         await interaction.response.defer()
-        if du.contains_roles(interaction.user.roles, "Moderator"):
-            channel_user = await du.get_user_by_original_message(interaction.channel)
-            await du.send_log_message(
+        assert isinstance(interaction.channel, discord.channel.TextChannel)
+        assert isinstance(interaction.user, discord.Member)
+
+        if utils.contains_roles(interaction.user.roles, "Moderator"):
+            channel_user = await utils.get_user_by_original_message(interaction.channel)
+            await utils.send_log_message(
                 interaction=interaction,
                 content=f"({interaction.channel.topic}) Ticket channel closed for user:\n{channel_user.display_name if channel_user else '?'} - {channel_user.mention if channel_user else '?'}",
                 mod=interaction.user,
@@ -250,9 +302,9 @@ class PViewSupportMessageCloseChannel(discord.ui.View):
 
 
 async def setup(bot: commands.Bot) -> None:
-    bot.add_view(PViewSupport())
-    bot.add_view(PViewVerify())
-    bot.add_view(PViewSupportGroup())
-    bot.add_view(PViewSupportNames())
-    bot.add_view(PViewSupportMessage())
-    bot.add_view(PViewSupportMessageCloseChannel())
+    bot.add_view(Support())
+    bot.add_view(Verify())
+    bot.add_view(SupportGroup())
+    bot.add_view(SupportNames())
+    bot.add_view(SupportMessage())
+    bot.add_view(SupportMessageCloseChannel())
--- hom/utils.py
+++ hom/utils.py
@@ -9,27 +9,32 @@ from discord.ext import commands
 from hom.cogs import views
 
 
-def get_role_by_name(guild: discord.Guild, role_name_to_find: str) -> t.Optional[discord.Role]:
-    roles = guild.roles
-    role_name_to_find = str(role_name_to_find).lower().replace(" ", "_").replace("-", "_")
-    for role in roles:
-        role_name = str(role).lower().replace(" ", "_").replace("-", "_")
-        if role_name == role_name_to_find:
+def normalize(obj: t.Any) -> str:
+    return str(obj).lower().replace(" ", "_").replace("-", "_")
+
+
+def get_role_by_name(
+    guild: discord.Guild, target_role: discord.Role | str
+) -> t.Optional[discord.Role]:
+    role_name_to_find = normalize(target_role)
+
+    for role in guild.roles:
+        if normalize(role) == role_name_to_find:
             return role
 
     return None
 
 
-def contains_roles(roles: t.List[discord.Role], *accepted_roles: discord.Role) -> bool:
-    lower_roles = [r.name.lower() for r in roles]
-    return any(str(r).lower() in lower_roles for r in accepted_roles) or not accepted_roles
+def contains_roles(roles: t.List[discord.Role], *accepted_roles: discord.Role | str) -> bool:
+    lower_roles = [normalize(r) for r in roles]
+    return any(normalize(r) in lower_roles for r in accepted_roles) or not accepted_roles
 
 
 def get_category(guild: discord.Guild, channel_name: str) -> t.Optional[discord.CategoryChannel]:
-    channel_name = channel_name.lower().replace("-", " ")
+    channel_name = normalize(channel_name)
     return discord.utils.find(
-        lambda c: str(c.name).lower().replace("-", " ") == channel_name,
-        guild.categories,  # JONX_TODO: Changed this to categories, is that right?
+        lambda c: normalize(c.name) == channel_name,
+        guild.categories,  # TODO: Changed this to categories, is that right?
     )
 
 
@@ -48,63 +53,73 @@ def get_channel_by_id(
     )
 
 
-def get_channel_by_name(bot, discord_guild_id: int, channel_name: str, channel_type: str = "text"):
-    if discord_guild_id not in [guild.id for guild in bot.guilds]:
+def get_channel_by_name(
+    bot: commands.Bot | discord.Client,
+    guild_id: int,
+    channel_name: str,
+    channel_type: str = "text",
+) -> t.Optional[discord.guild.GuildChannel]:
+    if not any(guild_id == guild.id for guild in bot.guilds):
+        return None
+
+    if not (guild := bot.get_guild(guild_id)):
         return None
 
     return discord.utils.find(
-        lambda c: str(c.name).lower().replace("-", " ")
-        == str(channel_name).lower().replace("-", " ")
+        lambda c: normalize(c.name) == normalize(channel_name)
         and str(c.type).lower() == channel_type,
-        bot.get_guild(int(discord_guild_id)).channels,
+        guild.channels,
     )
 
 
-async def get_user_by_original_message(channel):
-    channel_messages = [
-        message async for message in channel.history(limit=None, oldest_first=True)
-    ]
-    message_mentions = channel_messages[0].mentions if channel_messages else None
-    return message_mentions[0] if message_mentions else None
+async def get_user_by_original_message(
+    channel: discord.TextChannel,
+) -> t.Optional[discord.Member | discord.User]:
+    messages = [message async for message in channel.history(limit=1, oldest_first=True)]
+    mentions = messages[0].mentions if messages else None
+    return mentions[0] if mentions else None
 
 
-def get_user_ticket_channel(guild, user):
-    category = get_category(
-        discord_guild=guild, channel_name="Tickets"
-    )  # TODO: Make this channel_id?
+def get_user_ticket_channel(
+    guild: discord.Guild, user: discord.User | discord.Member
+) -> t.Optional[discord.guild.GuildChannel]:
+    if category := get_category(guild, "Tickets"):  # TODO: Make this channel_id?
+        for channel in category.channels:
+            user_perms = channel.overwrites_for(user)
 
-    if not category:
-        return
-
-    for channel in category.channels:
-        user_perms = channel.overwrites_for(user)
-        if user_perms.view_channel:
-            return channel
+            if user_perms.view_channel:
+                return channel
 
     return None
 
 
-async def create_ticket_for_user(interaction, instructions, button_label, example_url=None):
-    prevent_dupe_channels = True
+async def create_ticket_for_user(
+    interaction: discord.Interaction,
+    instructions: str,
+    button_label: t.Optional[str],
+    example_url: t.Optional[str] = None,
+) -> discord.TextChannel:
+    assert interaction.guild
+    assert isinstance(interaction.user, discord.Member)
     existing_ticket_channel = get_user_ticket_channel(interaction.guild, interaction.user)
 
-    if existing_ticket_channel and prevent_dupe_channels:
+    if existing_ticket_channel:
         msg_content = f":envelope:  Click [here]({existing_ticket_channel.jump_url}) to view your open ticket."
         await interaction.followup.send(content=msg_content, ephemeral=True)
-        return
+        return t.cast(discord.TextChannel, existing_ticket_channel)
 
-    tickets_category = get_category(
-        discord_guild=interaction.guild, channel_name="Tickets"
-    )  # TODO: Make this channel_id?
     channel_name = f"help-{interaction.user.display_name[:15]}"
-
+    tickets_category = get_category(interaction.guild, "Tickets")  # TODO: Make this channel_id?
     mod_role = get_role_by_name(interaction.guild, "Moderator")
     helpful_old_man_role = get_role_by_name(interaction.guild, "Helpful Old Man")
-    new_text_channel = await interaction.channel.guild.create_text_channel(
+    assert helpful_old_man_role
+    assert mod_role
+
+    new_text_channel = await interaction.guild.create_text_channel(
         name=channel_name,
         category=tickets_category,
         reason=f"{interaction.user.display_name} ({interaction.user.id}) has opened a ticket.",
-        topic=button_label,
+        topic=button_label or "Unknown (This is a bug)",
         overwrites={
             interaction.guild.default_role: discord.PermissionOverwrite(
                 read_messages=False,
@@ -119,58 +134,71 @@ async def create_ticket_for_user(interaction, instructions, button_label, exampl
         },
     )
 
-    msg_content = f":envelope:  We have created a support ticket for you, click [here]({new_text_channel.jump_url}) to view."
+    content = (
+        ":envelope:  We have created a support ticket for you, click [here]"
+        f"({new_text_channel.jump_url}) to view."
+    )
 
     embed = discord.Embed(description=instructions, title=button_label)
-    embed.set_image(url=example_url) if example_url else None
-
     embed.set_footer(
-        text="This channel is only visible to you and our moderators. If your question has been answered, feel free to close the ticket."
+        text=(
+            "This channel is only visible to you and our moderators. If your question has been "
+            "answered, feel free to close the ticket."
+        )
     )
-    await interaction.followup.send(content=msg_content, ephemeral=True)
+
+    if example_url:
+        embed.set_image(url=example_url)
+
+    await interaction.followup.send(content, ephemeral=True)
     await new_text_channel.send(
-        content=f"{interaction.user.mention}", embed=embed, view=vw.PViewSupport_Message()
+        f"{interaction.user.mention}", embed=embed, view=views.SupportMessage()
     )
 
-    await send_log_message(
-        interaction=interaction,
-        content=f"({new_text_channel.topic}) Ticket opened for user:\n``{interaction.user.display_name}`` - {interaction.user.mention}",
-        mod=interaction.client.user,
+    log_content = (
+        f"({new_text_channel.topic}) Ticket opened for user:\n``{interaction.user.display_name}`` "
+        f"- {interaction.user.mention}"
     )
 
+    assert interaction.client.user
+    await send_log_message(interaction, log_content, interaction.client.user)
     return new_text_channel
 
 
-async def send_log_message(interaction, content, mod, channel=None):
+async def send_log_message(
+    interaction: discord.Interaction,
+    content: str,
+    mod: discord.User | discord.ClientUser | discord.Member,
+    channel: t.Optional[discord.guild.GuildChannel] = None,
+) -> t.Optional[discord.Message]:
+    assert interaction.guild
     mod_logs_channel = get_channel_by_name(interaction.client, interaction.guild.id, "mod-logs")
     embed = discord.Embed(title=None, description=content)
     embed.set_footer(text=f"Mod: {mod.display_name}")
 
-    if channel:
+    if channel and mod_logs_channel:
         archived_data = await archive_channel_messages(channel)
         timestamp = datetime.datetime.now().strftime("_%Y_%m_%d_%Hh_%Mm_%Ss")
         file_name = f"{channel}" + timestamp + ".txt"
         buf = io.BytesIO(bytes(archived_data, "utf-8"))
         f = discord.File(buf, filename=file_name)
 
-    print(f"Message sent: {content} Footer: Mod: {mod.display_name}")
-    return (
-        await mod_logs_channel.send(embed=embed, file=f if channel else None)
-        if mod_logs_channel
-        else None
-    )
+        assert isinstance(mod_logs_channel, discord.TextChannel)
+        return await mod_logs_channel.send(embed=embed, file=f)
+
+    return None
 
 
-async def archive_channel_messages(channel_being_archived):
+async def archive_channel_messages(channel: discord.guild.GuildChannel) -> str:
+    assert isinstance(channel, discord.TextChannel)
     data = ""
+
     try:
         for message in [
-            message
-            async for message in channel_being_archived.history(limit=None, oldest_first=True)
+            message async for message in channel.history(limit=None, oldest_first=True)
         ]:
             datetime_info = datetime.datetime.strftime(message.created_at, "%b %d, %Y at %I:%M%p")
             datetime_info_timestamp = f"<t:{str(message.created_at.timestamp()).split('.')[0]}:F>"
-
             data += f"{datetime_info} {datetime_info_timestamp}\n{message.author.display_name.split('/')[0].strip()} - {message.author.id}\n{message.clean_content}\n\n"
 
     except:
