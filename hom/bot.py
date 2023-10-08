18a916b083f9b669d3f4cccbee9063ae62070407
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2023-10-08 13:39:03 -0700

    Add sync method to bot

--- hom/bot.py
+++ hom/bot.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 import discord
@@ -10,15 +9,16 @@ __all__ = ("Bot",)
 
 
 class Bot(commands.Bot):
-    __slots__ = ()
-
     def __init__(self) -> None:
         super().__init__(Constants.PREFIX, intents=discord.Intents.all())
 
     async def setup_hook(self) -> None:
         for path in Path("./hom/cogs").glob("[!_]*.py"):
-            await self.load_extension(f"hom.{path.stem}")
+            await self.load_extension(f"hom.cogs.{path.stem}")
 
     async def on_ready(self) -> None:
         user = self.user.display_name if self.user else "Bot"
         print(f"{user} has connected to Discord!")
+
+    async def sync(self, guild_id: int) -> None:
+        await self.tree.sync()
