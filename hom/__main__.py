2ced5f354d44253593d108da07195e4c02945101
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2023-10-08 13:42:03 -0700

    Slim down module entry point

--- hom/__main__.py
+++ hom/__main__.py
@@ -1,13 +1,7 @@
 import os
 
-import discord
-from discord.ext import commands
-from dotenv import load_dotenv
-
 from hom.bot import Bot
 from hom.config import Config
-
-load_dotenv()
 
 if __name__ == "__main__":
     if os.name != "nt":
@@ -17,14 +11,4 @@ if __name__ == "__main__":
         uvloop.install()
 
     bot = Bot()
-
-    @bot.command()
-    @commands.is_owner()
-    async def first_sync(ctx: commands.Context[commands.Bot]):
-        if ctx.guild:
-            await bot.tree.sync(guild=discord.Object(ctx.guild.id))
-            await ctx.channel.send("Original Sync Completed.")
-        else:
-            await ctx.channel.send("This command can only be run in a guild.")
-
     bot.run(Config.DISCORD_TOKEN)
