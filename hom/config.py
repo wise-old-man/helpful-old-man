cc51277e96a1e6c33aaea61b52c713936dc82c98
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2023-10-09 15:35:07 -0700

    Add additional config env vars

--- hom/config.py
+++ hom/config.py
@@ -15,6 +15,8 @@ class Config:
     DISCORD_TOKEN: t.Final[str] = environ["DISCORD_TOKEN"]
     SUPPORT_CHANNEL: t.Final[int] = int(environ["SUPPORT_CHANNEL"])
     TICKET_CATEGORY: t.Final[int] = int(environ["TICKET_CATEGORY"])
+    MOD_LOG_CHANNEL: t.Final[int] = int(environ["MOD_LOG_CHANNEL"])
+    MOD_ROLE: t.Final[int] = int(environ["MOD_ROLE"])
 
     def __init__(self) -> None:
         raise RuntimeError("Config should not be instantiated.")
