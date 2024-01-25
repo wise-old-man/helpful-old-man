e74ed3afa7dd4ef38cc75bf392bc597aec28a212
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2024-01-24 21:53:17 -0700

    Add patreon message back (#7)

--- hom/utils.py
+++ hom/utils.py
@@ -194,9 +194,10 @@ def build_support_embed(guild: discord.Guild) -> discord.Embed:
         f"\n\n**Groups** {Constants.ARROW} Assistance on group related things\n- Verify my group "
         "(for groups with 50+ members)\n- Reset my verification code\n- Remove me from a group\n"
         f"- Other\n\n**Name Changes** {Constants.ARROW} Help with name related things\n- Approve a"
-        "pending name change\n- Delete name change history\n- Other\n\n**API Key** "
-        f"{Constants.ARROW} Request an API key for development\n\n**Other** "
-        f"{Constants.ARROW} Request assistance for all other inquiries"
+        "pending name change\n- Delete name change history\n- Other\n\n**Patreon** "
+        f"{Constants.ARROW} Request help with Patreon benefits\n\n**API Key** {Constants.ARROW} "
+        f"Request an API key for development\n\n**Other** {Constants.ARROW} Request assistance "
+        "for all other inquiries"
     )
 
     footer = (
